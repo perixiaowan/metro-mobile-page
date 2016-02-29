9d4fd56ccd35b519971746c89c88584e51a9fd27
icemelo <frankfan.hu@gmail.com>
2016-02-29 17:23:47 +0800

    2
    2

--- js/app.js
+++ js/app.js
@@ -7,6 +7,27 @@ $(function () {
 	$("div#13 ").css("background-position","center");
 	$("div#13 ").css("background-size","cover");
 
+	var tranz = "translateZ("+wid/2+"px)";
+	var tranz2 = "rotateX(-90deg) "+"translateZ("+(-wid/2)+"px)";
 
-})
+	console.log("tranz :"+ tranz ,tranz2);
+	$(".flip").css("-webkit-transform", tranz);
+	$(".flop").css("-webkit-transform", tranz2);
+    /*-webkit-transform: rotateX(-90deg) translateZ(-60px);*/
 
+    var fliped = Boolean(false);
+	setInterval(rotate, 1000);
+
+	function rotate(){
+		if(!fliped){
+			$("#r11").css("-webkit-transform","rotateX(90deg)");
+			fliped = true;
+		}
+		else {
+			$("#r11").css("-webkit-transform","rotateX(0deg)");
+			fliped = false;
+		}
+	
+	}
+
+});
