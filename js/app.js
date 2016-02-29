83aaf50db7beb53b1aed261fe02a5e9be958606d
icemelo <frankfan.hu@gmail.com>
2016-02-29 13:41:06 +0800

    1
    1

--- js/app.js
+++ js/app.js
@@ -3,7 +3,10 @@ $(function () {
 	console.log("wid :" + wid);
 	$('.item-square').height(wid);
 	$('.item-rect').height(wid);
+	$("div#13 ").css("background-image","url(./images/banks.jpg)");
+	$("div#13 ").css("background-position","center");
+	$("div#13 ").css("background-size","cover");
+
 
 })
-
 
