256c9e5375a64feff015d76c5d4b3f5562e5b0b8
Yanbin Ma <yianbin@gmail.com>
2016-03-01 09:55:40 +0800

    Add modal

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 $(function () {
-	var wid = $('.item-square').width()
-	console.log("wid :" + wid);
-	$('.item-square').height(wid);
-	$('.item-rect').height(wid);
+	// init
+	var iw = $('.item-square').width();
+	var dw = $(window).width();
+	var dh = $(window).height();
+	console.log("iw :" + iw);
+	console.log("dh :" + dh);
+	$('.item-square').height(iw);
+	$('.item-rect').height(iw);
+
+	
+
+	$('.item-square, .item-rect').on('click', function () {
+		$('.modal').show();
+		$('body').addClass('modal-open')
+	})
 
 })
 
